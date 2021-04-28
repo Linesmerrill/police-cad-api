96c05eab5ce7d6253f7662bda91396ae0fa69079
Merrill Lines <linesmerrill@gmail.com>
2021-04-27 21:59:30 -0700

    move around database collections

--- config/logging.go
+++ config/logging.go
@@ -1,10 +1,14 @@
 package config
 
 import (
+	"fmt"
+	"net/http"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
 
+// Config holds the project config values
 type Config struct {
 	MongoClient mongo.Client
 }
@@ -16,4 +20,13 @@ func New() {
 	logger := zap.NewExample()
 	defer logger.Sync()
 	_ = zap.ReplaceGlobals(logger)
+}
+
+// ErrorStatus is a useful function that will log, write http headers and body for a
+// give message, status code and err
+func ErrorStatus(message string, httpStatusCode int, w http.ResponseWriter, err error) {
+	zap.S().With(err).Error(message)
+	w.WriteHeader(httpStatusCode)
+	w.Write([]byte(fmt.Sprintf(`{"response": "%s, %v"}`, message, err)))
+	return
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
 	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.6.1
 	go.mongodb.org/mongo-driver v1.5.1
 	go.uber.org/zap v1.16.0
 )
