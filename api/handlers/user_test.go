0f0bc885b3eb985822506e9f205511e7a54ddafc
Merrill Lines <linesmerrill@gmail.com>
2021-04-30 20:50:27 -0700

    add user handler tests

--- api/handlers/user_test.go
+++ api/handlers/user_test.go
@@ -1,59 +1,95 @@
 package handlers_test
 
 import (
-	"context"
-	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
+
+	"github.com/linesmerrill/police-cad-api/databases"
+	"github.com/linesmerrill/police-cad-api/databases/mocks"
+
 	"github.com/linesmerrill/police-cad-api/api/handlers"
-	"github.com/linesmerrill/police-cad-api/models"
 )
 
-var a handlers.App
-
-type userDBMock struct {
-	isError  bool
-	objectID string
+type MockDatabaseHelper struct {
+	mock.Mock
 }
 
-func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+// Client provides a mock function.
+func (_m *MockDatabaseHelper) Client() databases.ClientHelper {
+	ret := _m.Called()
+
+	var r0 databases.ClientHelper
+	if rf, ok := ret.Get(0).(func() databases.ClientHelper); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(databases.ClientHelper)
+		}
+	}
+
+	return r0
+}
+
+// Collection provides a mock function.
+func (_m *MockDatabaseHelper) Collection(name string) databases.CollectionHelper {
+	ret := _m.Called(name)
+
+	var r0 databases.CollectionHelper
+	if rf, ok := ret.Get(0).(func(string) databases.CollectionHelper); ok {
+		r0 = rf(name)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(databases.CollectionHelper)
+		}
+	}
+
+	return r0
+}
+
+func TestUser_UserHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/v1/user/asdf", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer abc123")
+
+	var db databases.DatabaseHelper
+	var client databases.ClientHelper
+	var conn databases.CollectionHelper
+	var singleResultHelper databases.SingleResultHelper
+
+	db = &MockDatabaseHelper{} // can be used as db = &mocks.DatabaseHelper{}
+	client = &mocks.ClientHelper{}
+	conn = &mocks.CollectionHelper{}
+	singleResultHelper = &mocks.SingleResultHelper{}
+
+	client.(*mocks.ClientHelper).On("StartSession").Return(nil, errors.New("mocked-error"))
+	db.(*MockDatabaseHelper).On("Client").Return(client)
+	singleResultHelper.(*mocks.SingleResultHelper).On("Decode", mock.Anything).Return(errors.New("mocked-error"))
+	conn.(*mocks.CollectionHelper).On("FindOne", mock.Anything, mock.Anything).Return(singleResultHelper)
+	db.(*MockDatabaseHelper).On("Collection", "users").Return(conn)
+
+	userDatabase := databases.NewUserDatabase(db)
+	u := handlers.User{
+		DB: userDatabase,
+	}
+
 	rr := httptest.NewRecorder()
-	a.Router.ServeHTTP(rr, req)
-	return rr
-}
+	handler := http.HandlerFunc(u.UserHandler)
 
-func checkResponseCode(t *testing.T, expected, actual int) {
-	if expected != actual {
-		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
-	}
-}
+	handler.ServeHTTP(rr, req)
 
-func (u userDBMock) FindOne(context.Context, interface{}) (*models.User, error) {
-	if u.isError {
-		return nil, errors.New("some error")
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	userInner := models.UserInner{
-		Email:    "test@test.com",
-		Username: "testUser",
-	}
-	return &models.User{ID: u.objectID, UserInner: userInner}, nil
-}
-
-func TestApp_UserHandlerInvalidToken(t *testing.T) {
-	a.Router = a.New()
-	req, _ := http.NewRequest("GET", "/api/v1/user/1234", nil)
-	req.Header.Add("Authorization", "Bearer asdfasdf")
-	response := executeRequest(req)
-
-	checkResponseCode(t, http.StatusInternalServerError, response.Code)
-
-	var m map[string]string
-	json.Unmarshal(response.Body.Bytes(), &m)
-	if m["error"] != "failed to parse token, token contains an invalid number of segments" {
-		t.Errorf("Expected the 'error' key of the reponse to be set to 'failed to parse token, token contains an invalid number of segments'. Got '%s'", m["error"])
+	expected := `{"response": "failed to get objectID from Hex, the provided hex string is not a valid ObjectID"}{"response": "failed to get user by ID, mocked-error"}null`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
