cc5f25ba2463fd852b98892454f439e44524d60d
Merrill Lines <linesmerrill@gmail.com>
2021-04-30 21:46:17 -0700

    add json test cases

--- api/handlers/community_test.go
+++ api/handlers/community_test.go
@@ -6,6 +6,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/linesmerrill/police-cad-api/models"
+
 	"github.com/stretchr/testify/mock"
 
 	"github.com/linesmerrill/police-cad-api/databases"
@@ -58,6 +60,58 @@ func TestCommunity_CommunityHandler(t *testing.T) {
 	}
 }
 
+func TestCommunity_CommunityHandlerMarshalError(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/v1/community/1234", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer abc123")
+
+	var db databases.DatabaseHelper
+	var client databases.ClientHelper
+	var conn databases.CollectionHelper
+	var singleResultHelper databases.SingleResultHelper
+
+	db = &MockDatabaseHelper{} // can be used as db = &mocks.DatabaseHelper{}
+	client = &mocks.ClientHelper{}
+	conn = &mocks.CollectionHelper{}
+	singleResultHelper = &mocks.SingleResultHelper{}
+
+	x := map[string]interface{}{
+		"foo": make(chan int),
+	}
+
+	client.(*mocks.ClientHelper).On("StartSession").Return(nil, errors.New("mocked-error"))
+	db.(*MockDatabaseHelper).On("Client").Return(client)
+	singleResultHelper.(*mocks.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		arg := args.Get(0).(**models.Community)
+		(*arg).CommunityInner.ActivePanics = x
+
+	})
+	conn.(*mocks.CollectionHelper).On("FindOne", mock.Anything, mock.Anything).Return(singleResultHelper)
+	db.(*MockDatabaseHelper).On("Collection", "communities").Return(conn)
+
+	communityDatabase := databases.NewCommunityDatabase(db)
+	u := handlers.Community{
+		DB: communityDatabase,
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(u.CommunityHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := `{"response": "failed to get objectID from Hex, the provided hex string is not a valid ObjectID"}{"response": "failed to marshal response, json: unsupported type: chan int"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
 func TestCommunity_CommunityByOwnerHandlerBadRequestError(t *testing.T) {
 	req, err := http.NewRequest("GET", "/api/v1/community/608cafe595eb9dc05379b7f4/608cafd695eb9dc05379b7f3", nil)
 	if err != nil {
@@ -97,6 +151,107 @@ func TestCommunity_CommunityByOwnerHandlerBadRequestError(t *testing.T) {
 	}
 
 	expected := `{"response": "failed to get objectID from Hex, the provided hex string is not a valid ObjectID"}{"response": "failed to get community by ID and ownerID, mocked-error"}null`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestCommunity_CommunityByOwnerHandlerJsonMarshalError(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/v1/community/608cafe595eb9dc05379b7f4/608cafd695eb9dc05379b7f3", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer abc123")
+
+	var db databases.DatabaseHelper
+	var client databases.ClientHelper
+	var conn databases.CollectionHelper
+	var singleResultHelper databases.SingleResultHelper
+
+	db = &MockDatabaseHelper{} // can be used as db = &mocks.DatabaseHelper{}
+	client = &mocks.ClientHelper{}
+	conn = &mocks.CollectionHelper{}
+	singleResultHelper = &mocks.SingleResultHelper{}
+
+	x := map[string]interface{}{
+		"foo": make(chan int),
+	}
+
+	client.(*mocks.ClientHelper).On("StartSession").Return(nil, errors.New("mocked-error"))
+	db.(*MockDatabaseHelper).On("Client").Return(client)
+	singleResultHelper.(*mocks.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		arg := args.Get(0).(**models.Community)
+		(*arg).CommunityInner.ActivePanics = x
+
+	})
+	conn.(*mocks.CollectionHelper).On("FindOne", mock.Anything, mock.Anything).Return(singleResultHelper)
+	db.(*MockDatabaseHelper).On("Collection", "communities").Return(conn)
+
+	communityDatabase := databases.NewCommunityDatabase(db)
+	u := handlers.Community{
+		DB: communityDatabase,
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(u.CommunityByOwnerHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+
+	expected := `{"response": "failed to get objectID from Hex, the provided hex string is not a valid ObjectID"}{"response": "failed to marshal response, json: unsupported type: chan int"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestCommunity_CommunityByOwnerHandlerMultiError(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/v1/community/608cafe595eb9dc05379b7f4/608cafd695eb9dc05379b7f3", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer abc123")
+
+	var db databases.DatabaseHelper
+	var client databases.ClientHelper
+	var conn databases.CollectionHelper
+	var singleResultHelper databases.SingleResultHelper
+
+	db = &MockDatabaseHelper{} // can be used as db = &mocks.DatabaseHelper{}
+	client = &mocks.ClientHelper{}
+	conn = &mocks.CollectionHelper{}
+	singleResultHelper = &mocks.SingleResultHelper{}
+
+	//resp := models.Community{}
+
+	client.(*mocks.ClientHelper).On("StartSession").Return(nil, errors.New("mocked-error"))
+	db.(*MockDatabaseHelper).On("Client").Return(client)
+	singleResultHelper.(*mocks.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		arg := args.Get(0).(**models.Community)
+		(*arg).ID = "mocked-user"
+	})
+	conn.(*mocks.CollectionHelper).On("FindOne", mock.Anything, mock.Anything).Return(singleResultHelper)
+	db.(*MockDatabaseHelper).On("Collection", "communities").Return(conn)
+
+	communityDatabase := databases.NewCommunityDatabase(db)
+	u := handlers.Community{
+		DB: communityDatabase,
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(u.CommunityByOwnerHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+
+	expected := `{"response": "failed to get objectID from Hex, the provided hex string is not a valid ObjectID"}{"_id":"mocked-user","community":{"name":"","ownerID":"","code":"","activePanics":null,"activeSignal100":false,"createdAt":"1969-12-31T17:00:00-07:00","updatedAt":"1969-12-31T17:00:00-07:00"},"__v":0}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
