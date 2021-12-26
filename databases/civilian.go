a7482d333afd77032eb50550a620186a9afa6dbc
Merrill Lines <linesmerrill@gmail.com>
2021-12-25 21:15:43 -0700

    add new civilian routes to fetch by ID and return all

---
+++ databases/civilian.go
@@ -0,0 +1,46 @@
+package databases
+
+//go generate: mockery --name UserDatabase
+
+import (
+	"context"
+
+	"github.com/linesmerrill/police-cad-api/models"
+)
+
+const civilianName = "civilians"
+
+// CivilianDatabase contains the methods to use with the civilian database
+type CivilianDatabase interface {
+	FindOne(ctx context.Context, filter interface{}) (*models.Civilian, error)
+	Find(ctx context.Context, filter interface{}) ([]models.Civilian, error)
+}
+
+type civilianDatabase struct {
+	db DatabaseHelper
+}
+
+// NewCivilianDatabase initializes a new instance of user database with the provided db connection
+func NewCivilianDatabase(db DatabaseHelper) CivilianDatabase {
+	return &civilianDatabase{
+		db: db,
+	}
+}
+
+func (c *civilianDatabase) FindOne(ctx context.Context, filter interface{}) (*models.Civilian, error) {
+	civilian := &models.Civilian{}
+	err := c.db.Collection(civilianName).FindOne(ctx, filter).Decode(&civilian)
+	if err != nil {
+		return nil, err
+	}
+	return civilian, nil
+}
+
+func (c *civilianDatabase) Find(ctx context.Context, filter interface{}) ([]models.Civilian, error) {
+	var civilians []models.Civilian
+	err := c.db.Collection(civilianName).Find(ctx, filter).Decode(&civilians)
+	if err != nil {
+		return nil, err
+	}
+	return civilians, nil
+}
