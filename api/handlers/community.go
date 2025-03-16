7028a4c7d6ef495436f433ab09968bd0e7e8aa7c
Merrill Lines <linesmerrill@gmail.com>
2025-03-15 23:34:11 -0700

    Fix empty community array

--- api/handlers/community.go
+++ api/handlers/community.go
@@ -150,9 +150,12 @@ func (c Community) CreateCommunityHandler(w http.ResponseWriter, r *http.Request
 		Status:      "approved",
 	}
 
-	// Update the user's communities array
+	// Ensure the user's communities array is initialized
 	filter := bson.M{"_id": uID}
-	update := bson.M{"$addToSet": bson.M{"user.communities": newUserCommunity}} // $addToSet ensures no duplicates
+	update := bson.M{
+		"$set":      bson.M{"user.communities": bson.M{"$ifNull": []models.UserCommunity{}}},
+		"$addToSet": bson.M{"user.communities": newUserCommunity}, // $addToSet ensures no duplicates
+	}
 	_, err = c.UDB.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		config.ErrorStatus("failed to update user's communities", http.StatusInternalServerError, w, err)
