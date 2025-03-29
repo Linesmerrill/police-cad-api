9a12a69ab19b932b7c4c8c8e2788e1e9dc729604
Merrill Lines <linesmerrill@gmail.com>
2025-03-29 16:51:10 -0700

    Add new members to departments

--- api/handlers/community.go
+++ api/handlers/community.go
@@ -1347,26 +1347,8 @@ func (c Community) UpdateDepartmentMembersHandler(w http.ResponseWriter, r *http
 		return
 	}
 
-	// Initialize the members field to an empty array if it is null or does not exist
-	filter := bson.M{"_id": cID, "community.departments._id": dID, "$or": []bson.M{
-		{"community.departments.members": bson.M{"$exists": false}},
-		{"community.departments.members": nil},
-	}}
-	update := bson.M{"$set": bson.M{"community.departments.$.members": []bson.M{}}}
-	err = c.DB.UpdateOne(context.Background(), filter, update)
-	if err != nil {
-		config.ErrorStatus("failed to initialize department members", http.StatusInternalServerError, w, err)
-		return
-	}
-
-	members := make([]bson.M, len(requestBody.Members))
-	for i, memberID := range requestBody.Members {
-		mID := primitive.NewObjectID()
-		members[i] = bson.M{"_id": mID, "status": "approved", "userID": memberID}
-	}
-
-	filter = bson.M{"_id": cID, "community.departments._id": dID}
-	update = bson.M{"$addToSet": bson.M{"community.departments.$.members": bson.M{"$each": members}}}
+	filter := bson.M{"_id": cID, "community.departments._id": dID}
+	update := bson.M{"$addToSet": bson.M{"community.departments.$.members": bson.M{"$each": requestBody.Members}}}
 	err = c.DB.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		config.ErrorStatus("failed to update department members", http.StatusInternalServerError, w, err)
