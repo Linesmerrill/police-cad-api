1cac23463f3155971584b85c417d463fac59cfe2
Merrill Lines <linesmerrill@gmail.com>
2025-04-22 12:21:33 -0700

    Add new members counter

--- api/handlers/community.go
+++ api/handlers/community.go
@@ -83,6 +83,7 @@ func (c Community) CreateCommunityHandler(w http.ResponseWriter, r *http.Request
 	newCommunity.Details.Departments = []models.Department{}
 	newCommunity.Details.TenCodes = defaultTenCodes()
 	newCommunity.Details.Fines = defaultCommunityFines()
+	newCommunity.Details.MembersCount = 1
 
 	// Initialize the events slice if it is null
 	if newCommunity.Details.Events == nil {
