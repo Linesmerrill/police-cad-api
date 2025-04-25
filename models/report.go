c039d2d2465f609904e17df1551a7e9a165aeba6
Merrill Lines <linesmerrill@gmail.com>
2025-04-25 08:18:09 -0700

    Update report model

--- models/report.go
+++ models/report.go
@@ -4,9 +4,11 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Report represents a player report
 type Report struct {
-	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	UserID     string             `bson:"userId" json:"userId"`
-	ReportType string             `bson:"reportType" json:"reportType"`
-	CreatedAt  primitive.DateTime `bson:"createdAt" json:"createdAt"`
-	ReportedBy string             `bson:"reportedBy" json:"reportedBy"`
+	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	ItemID            string             `bson:"itemId" json:"itemId"`               // The ID of the item being reported, userID, communityID, etc.
+	ItemType          ItemType           `bson:"itemType" json:"itemType"`           // user report, ad report, etc.
+	ReportedIssue     string             `bson:"reportedIssue" json:"reportedIssue"` // hate, scan, etc.
+	AdditionalDetails string             `bson:"additionalDetails" json:"additionalDetails"`
+	CreatedAt         primitive.DateTime `bson:"createdAt" json:"createdAt"`
+	ReportedByID      string             `bson:"reportedById" json:"reportedById"`
 }
