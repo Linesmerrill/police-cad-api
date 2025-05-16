19ba9bab20a198c5763e05b37b341b9e47a5bab5
Merrill Lines <linesmerrill@gmail.com>
2025-05-16 11:18:23 -0700

    Remove dupe adding members to community

--- api/handlers/license_test.go
+++ api/handlers/license_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/linesmerrill/police-cad-api/api/handlers"
 	"github.com/linesmerrill/police-cad-api/databases"
@@ -186,7 +187,11 @@ func TestLicense_LicenseByIDHandlerSuccess(t *testing.T) {
 	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
 	singleResultHelper.(*mocksdb.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
 		arg := args.Get(0).(**models.License)
-		(*arg).ID = "5fc51f58c72ff10004dca382"
+		cID, err := primitive.ObjectIDFromHex("5fc51f58c72ff10004dca382")
+		if err != nil {
+			t.Fatal(err)
+		}
+		(*arg).ID = cID
 
 	})
 	conn.(*mocksdb.CollectionHelper).On("FindOne", mock.Anything, mock.Anything).Return(singleResultHelper)
@@ -210,197 +215,6 @@ func TestLicense_LicenseByIDHandlerSuccess(t *testing.T) {
 	_ = json.Unmarshal(rr.Body.Bytes(), &testLicense)
 
 	assert.Equal(t, "5fc51f58c72ff10004dca382", testLicense.ID)
-}
-
-func TestLicense_LicenseHandlerJsonMarshalError(t *testing.T) {
-	req, err := http.NewRequest("GET", "/api/v1/licenses", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req.Header.Set("Authorization", "Bearer abc123")
-
-	var db databases.DatabaseHelper
-	var client databases.ClientHelper
-	var conn databases.CollectionHelper
-	var singleResultHelper databases.SingleResultHelper
-
-	db = &mocksdb.DatabaseHelper{} // can be used as db = &mocksdb.DatabaseHelper{}
-	client = &mocksdb.ClientHelper{}
-	conn = &mocksdb.CollectionHelper{}
-	singleResultHelper = &mocksdb.SingleResultHelper{}
-
-	x := map[string]interface{}{
-		"foo": make(chan int),
-	}
-
-	client.(*mocksdb.ClientHelper).On("StartSession").Return(nil, errors.New("mocked-error"))
-	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
-	singleResultHelper.(*mocksdb.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
-		arg := args.Get(0).(*[]models.License)
-		*arg = []models.License{{Details: models.LicenseDetails{CreatedAt: x}}}
-	})
-	conn.(*mocksdb.CollectionHelper).On("Find", mock.Anything, mock.Anything, mock.Anything).Return(singleResultHelper)
-	db.(*mocksdb.DatabaseHelper).On("Collection", "licenses").Return(conn)
-
-	licenseDatabase := databases.NewLicenseDatabase(db)
-	u := handlers.License{
-		DB: licenseDatabase,
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicenseHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusInternalServerError {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
-
-	expected := models.ErrorMessageResponse{Response: models.MessageError{Message: "failed to marshal response", Error: "json: unsupported type: chan int"}}
-	b, _ := json.Marshal(expected)
-	if rr.Body.String() != string(b) {
-		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-	}
-}
-
-func TestLicense_LicenseHandlerFailedToFindOne(t *testing.T) {
-	req, err := http.NewRequest("GET", "/api/v1/licenses", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req.Header.Set("Authorization", "Bearer abc123")
-
-	var db databases.DatabaseHelper
-	var client databases.ClientHelper
-	var conn databases.CollectionHelper
-	var singleResultHelper databases.SingleResultHelper
-
-	db = &mocksdb.DatabaseHelper{} // can be used as db = &mocksdb.DatabaseHelper{}
-	client = &mocksdb.ClientHelper{}
-	conn = &mocksdb.CollectionHelper{}
-	singleResultHelper = &mocksdb.SingleResultHelper{}
-
-	client.(*mocksdb.ClientHelper).On("StartSession").Return(nil, errors.New("mocked-error"))
-	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
-	singleResultHelper.(*mocksdb.SingleResultHelper).On("Decode", mock.Anything).Return(errors.New("mongo: no documents in result"))
-	conn.(*mocksdb.CollectionHelper).On("Find", mock.Anything, mock.Anything, mock.Anything).Return(singleResultHelper)
-	db.(*mocksdb.DatabaseHelper).On("Collection", "licenses").Return(conn)
-
-	licenseDatabase := databases.NewLicenseDatabase(db)
-	u := handlers.License{
-		DB: licenseDatabase,
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicenseHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusNotFound {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
-	}
-
-	expected := models.ErrorMessageResponse{Response: models.MessageError{Message: "failed to get licenses", Error: "mongo: no documents in result"}}
-	b, _ := json.Marshal(expected)
-	if rr.Body.String() != string(b) {
-		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-	}
-}
-
-func TestLicense_LicenseHandlerSuccess(t *testing.T) {
-	req, err := http.NewRequest("GET", "/api/v1/licenses", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req.Header.Set("Authorization", "Bearer abc123")
-
-	var db databases.DatabaseHelper
-	var client databases.ClientHelper
-	var conn databases.CollectionHelper
-	var singleResultHelper databases.SingleResultHelper
-
-	db = &mocksdb.DatabaseHelper{} // can be used as db = &mocksdb.DatabaseHelper{}
-	client = &mocksdb.ClientHelper{}
-	conn = &mocksdb.CollectionHelper{}
-	singleResultHelper = &mocksdb.SingleResultHelper{}
-
-	client.(*mocksdb.ClientHelper).On("StartSession").Return(nil, errors.New("mocked-error"))
-	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
-	singleResultHelper.(*mocksdb.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
-		arg := args.Get(0).(*[]models.License)
-		*arg = []models.License{{ID: "5fc51f58c72ff10004dca382"}}
-
-	})
-	conn.(*mocksdb.CollectionHelper).On("Find", mock.Anything, mock.Anything, mock.Anything).Return(singleResultHelper)
-	db.(*mocksdb.DatabaseHelper).On("Collection", "licenses").Return(conn)
-
-	licenseDatabase := databases.NewLicenseDatabase(db)
-	u := handlers.License{
-		DB: licenseDatabase,
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicenseHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
-
-	var testLicense []models.License
-	_ = json.Unmarshal(rr.Body.Bytes(), &testLicense)
-
-	assert.Equal(t, "5fc51f58c72ff10004dca382", testLicense[0].ID)
-}
-
-func TestLicense_LicenseHandlerEmptyResponse(t *testing.T) {
-	req, err := http.NewRequest("GET", "/api/v1/license", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req.Header.Set("Authorization", "Bearer abc123")
-
-	var db databases.DatabaseHelper
-	var client databases.ClientHelper
-	var conn databases.CollectionHelper
-	var cursorHelper databases.CursorHelper
-
-	db = &mocksdb.DatabaseHelper{} // can be used as db = &mocksdb.DatabaseHelper{}
-	client = &mocksdb.ClientHelper{}
-	conn = &mocksdb.CollectionHelper{}
-	cursorHelper = &mocksdb.CursorHelper{}
-
-	client.(*mocksdb.ClientHelper).On("StartSession").Return(nil, errors.New("mocked-error"))
-	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
-	cursorHelper.(*mocksdb.CursorHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
-		arg := args.Get(0).(*[]models.License)
-		*arg = nil
-	})
-	conn.(*mocksdb.CollectionHelper).On("Find", mock.Anything, mock.Anything, mock.Anything).Return(cursorHelper)
-	db.(*mocksdb.DatabaseHelper).On("Collection", "licenses").Return(conn)
-
-	licenseDatabase := databases.NewLicenseDatabase(db)
-	u := handlers.License{
-		DB: licenseDatabase,
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicenseHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
-
-	expected := "[]"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: \ngot: %v \nwant: %v", rr.Body.String(), expected)
-	}
 }
 
 func TestLicense_LicensesByUserIDHandlerJsonMarshalError(t *testing.T) {
@@ -439,7 +253,7 @@ func TestLicense_LicensesByUserIDHandlerJsonMarshalError(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByUserIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -484,7 +298,7 @@ func TestLicense_LicensesByUserIDHandlerFailedToFindOne(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByUserIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -529,7 +343,7 @@ func TestLicense_LicensesByUserIDHandlerActiveCommunityIDFailedToFindOne(t *test
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByUserIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -566,7 +380,11 @@ func TestLicense_LicensesByUserIDHandlerSuccess(t *testing.T) {
 	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
 	singleResultHelper.(*mocksdb.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
 		arg := args.Get(0).(*[]models.License)
-		*arg = []models.License{{ID: "5fc51f36c72ff10004dca381"}}
+		cID, err := primitive.ObjectIDFromHex("5fc51f36c72ff10004dca381")
+		if err != nil {
+			t.Fatal(err)
+		}
+		*arg = []models.License{{ID: cID}}
 
 	})
 	conn.(*mocksdb.CollectionHelper).On("Find", mock.Anything, mock.Anything, mock.Anything).Return(singleResultHelper)
@@ -578,7 +396,7 @@ func TestLicense_LicensesByUserIDHandlerSuccess(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByUserIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -614,7 +432,11 @@ func TestLicense_LicensesByUserIDHandlerSuccessWithActiveCommunityID(t *testing.
 	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
 	singleResultHelper.(*mocksdb.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
 		arg := args.Get(0).(*[]models.License)
-		*arg = []models.License{{ID: "5fc51f36c72ff10004dca381"}}
+		cID, err := primitive.ObjectIDFromHex("5fc51f36c72ff10004dca381")
+		if err != nil {
+			t.Fatal(err)
+		}
+		*arg = []models.License{{ID: cID}}
 
 	})
 	conn.(*mocksdb.CollectionHelper).On("Find", mock.Anything, mock.Anything, mock.Anything).Return(singleResultHelper)
@@ -626,7 +448,7 @@ func TestLicense_LicensesByUserIDHandlerSuccessWithActiveCommunityID(t *testing.
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByUserIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -662,7 +484,11 @@ func TestLicense_LicensesByUserIDHandlerSuccessWithNullCommunityID(t *testing.T)
 	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
 	singleResultHelper.(*mocksdb.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
 		arg := args.Get(0).(*[]models.License)
-		*arg = []models.License{{ID: "5fc51f36c72ff10004dca381"}}
+		cID, err := primitive.ObjectIDFromHex("5fc51f36c72ff10004dca381")
+		if err != nil {
+			t.Fatal(err)
+		}
+		*arg = []models.License{{ID: cID}}
 
 	})
 	conn.(*mocksdb.CollectionHelper).On("Find", mock.Anything, mock.Anything, mock.Anything).Return(singleResultHelper)
@@ -674,7 +500,7 @@ func TestLicense_LicensesByUserIDHandlerSuccessWithNullCommunityID(t *testing.T)
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByUserIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -721,7 +547,7 @@ func TestLicense_LicensesByUserIDHandlerEmptyResponse(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByUserIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -771,7 +597,7 @@ func TestLicense_LicensesByOwnerIDHandlerJsonMarshalError(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByOwnerIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -816,7 +642,7 @@ func TestLicense_LicensesByOwnerIDHandlerFailedToFindOne(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByOwnerIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -853,7 +679,11 @@ func TestLicense_LicensesByOwnerIDHandlerSuccess(t *testing.T) {
 	db.(*mocksdb.DatabaseHelper).On("Client").Return(client)
 	singleResultHelper.(*mocksdb.SingleResultHelper).On("Decode", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
 		arg := args.Get(0).(*[]models.License)
-		*arg = []models.License{{ID: "5fc51f36c72ff10004dca381"}}
+		cID, err := primitive.ObjectIDFromHex("5fc51f36c72ff10004dca381")
+		if err != nil {
+			t.Fatal(err)
+		}
+		*arg = []models.License{{ID: cID}}
 
 	})
 	conn.(*mocksdb.CollectionHelper).On("Find", mock.Anything, mock.Anything, mock.Anything).Return(singleResultHelper)
@@ -865,7 +695,7 @@ func TestLicense_LicensesByOwnerIDHandlerSuccess(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByOwnerIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -912,7 +742,7 @@ func TestLicense_LicensesByOwnerIDHandlerEmptyResponse(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(u.LicensesByOwnerIDHandler)
+	handler := http.HandlerFunc(u.LicensesByCivilianIDHandler)
 
 	handler.ServeHTTP(rr, req)
 
