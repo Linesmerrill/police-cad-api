96b968436b0f4b2d3200541e3d4e533af2f06cec
Merrill Lines <linesmerrill@gmail.com>
2025-08-18 15:34:42 -0700

    Remove remaining debug route references to fix compilation

--- api/handlers/api.go
+++ api/handlers/api.go
@@ -87,10 +87,6 @@ func (a *App) New() *mux.Router {
 	apiCreate.Handle("/admin/users/{id}/reset-password", http.HandlerFunc(adminHandler.AdminUserResetPasswordHandler)).Methods("POST")
 	apiCreate.Handle("/admin/users/{id}/temp-password", http.HandlerFunc(adminHandler.AdminUserTempPasswordHandler)).Methods("POST")
 	
-	// Admin debug routes
-	apiCreate.Handle("/admin/debug/users", http.HandlerFunc(adminHandler.AdminDebugUsersHandler)).Methods("GET")
-	apiCreate.Handle("/admin/debug/communities", http.HandlerFunc(adminHandler.AdminDebugCommunitiesHandler)).Methods("GET")
-
 	apiCreate.Handle("/verify/send-verification-code", http.HandlerFunc(pv.CreatePendingVerificationHandler)).Methods("POST")
 	apiCreate.Handle("/verify/verify-code", http.HandlerFunc(pv.VerifyCodeHandler)).Methods("POST")
 	apiCreate.Handle("/verify/resend-verification-code", http.HandlerFunc(pv.ResendVerificationCodeHandler)).Methods("POST")
